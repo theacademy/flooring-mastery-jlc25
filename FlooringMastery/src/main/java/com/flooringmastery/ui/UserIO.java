37a68f13c9252acb203e8bb466bc3255301fb186
jlc25 <jinglingchen25@gmail.com>
2026-05-07 09:58:14 -0400

    Implemented UserIO

--- FlooringMastery/src/main/java/com/flooringmastery/ui/UserIO.java
+++ FlooringMastery/src/main/java/com/flooringmastery/ui/UserIO.java
@@ -1,4 +1,19 @@
 package com.flooringmastery.ui;
 
+import java.math.BigDecimal;
+
 public interface UserIO {
+
+    void print(String msg);
+
+    String readString(String prompt);
+
+    Integer readInt(String prompt);
+
+    Integer readInt(String prompt, Integer min, Integer max);
+
+    BigDecimal readBigDecimal(String prompt);
+
+    BigDecimal readBigDecimal(String prompt, BigDecimal min, BigDecimal max);
+
 }
--- FlooringMastery/src/main/java/com/flooringmastery/ui/UserIOConsoleImpl.java
+++ FlooringMastery/src/main/java/com/flooringmastery/ui/UserIOConsoleImpl.java
@@ -1,4 +1,67 @@
 package com.flooringmastery.ui;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class UserIOConsoleImpl implements UserIO{
+    Scanner input = new Scanner(System.in);
+    String stringInput;
+
+    @Override
+    public void print(String message){
+        System.out.println(message);
+    }
+
+
+    public String readString(String prompt){
+        System.out.println(prompt);
+        stringInput = input.nextLine();
+
+        return stringInput;
+    }
+
+    @Override
+    public Integer readInt(String prompt) {
+        System.out.println(prompt);
+        stringInput = input.nextLine();
+        return Integer.parseInt(stringInput);
+    }
+
+    @Override
+    public Integer readInt(String prompt, Integer min, Integer max) {
+        Integer bigDecimalInput = min - 1;
+
+        // Keep prompting the user until the input is valid (within the min-max range)
+        while(bigDecimalInput<min || bigDecimalInput>max){
+            System.out.println(prompt);
+            stringInput = input.nextLine();
+            bigDecimalInput = Integer.parseInt(stringInput);
+        }
+
+        return bigDecimalInput;
+    }
+
+    @Override
+    public BigDecimal readBigDecimal(String prompt) {
+        System.out.println(prompt);
+        stringInput = input.nextLine();
+
+        return new BigDecimal(stringInput);
+    }
+
+    @Override
+    public BigDecimal readBigDecimal(String prompt, BigDecimal min, BigDecimal max) {
+        // Initialize the Big Decimal to be out of bounds
+        BigDecimal bigDecimalInput = min.subtract(new BigDecimal("1"));
+
+        // Keep prompting the user until the input is valid (within the min-max range)
+        while(bigDecimalInput.compareTo(min) < 0 || bigDecimalInput.compareTo(max) > 0){
+            System.out.println(prompt);
+            stringInput = input.nextLine();
+            bigDecimalInput = new BigDecimal(stringInput);
+        }
+
+        return bigDecimalInput;
+    }
+
 }
